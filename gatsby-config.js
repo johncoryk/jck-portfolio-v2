e9a3bc74509e3e0360de2d8ae9cb8dcd21d4f235
Cory Kelley <coryk0789@gmail.com>
2020-05-13 09:30:15 -0400

    Deleted env variables

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,13 +22,5 @@ module.exports = {
         icon: `src/images/ckLogo_lg.png`,
       },
     },
-    {
-      resolve: `gatsby-source-contentful`,
-      options: {
-        spaceId: process.env.CONTENTFUL_SPACE_ID,
-        // Learn about environment variables: https://gatsby.dev/env-vars
-        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-      },
-    },
   ],
 }
